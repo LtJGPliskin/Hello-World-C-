7976bb367567d9ffced52263fdbdda55ca7c7817
LtJGPliskin <boucher@Austins-MacBook-Pro.local>
2016-02-23 12:42:16 -0700

    Uploading Code

---
+++ Boucher_Austin_lab0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * Austin Boucher
+ * Lab 0, CSCI 112
+ * 01/28/2015
+ */
+
+
+int main(void){
+	// printso "Hell World"
+  printf("Hello World\n");
+  //exits the program with no errors
+  return(0);
+}
+
